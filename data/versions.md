5c144278380c1ba8cd0c108c3cd9d7d0433aafb0
Anže Bertoncelj <anze.bb@hotmail.com>
2018-07-15 10:45:09 +0200

    Small helper program to parse sentneces

--- data/versions.md
+++ data/versions.md
@@ -1,2 +1,3 @@
-v2: delete all the auido files that user reported and all the audio files with _e.mp3 at the end meading they were english sentences
-v3: fix all the japanespod101 audio that was paresed incorrectly
+**v2**: delete all the auido files that user reported and all the audio files with _e.mp3 at the end meading they were english sentences
+
+**v3**: fix all the japanespod101 audio that was paresed incorrectly
--- parsedata/parse.js
+++ parsedata/parse.js
@@ -1,38 +1,38 @@
 fs = require('fs')
-fs.readFile('../data/all.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var jsonData = JSON.parse(data);
-  console.log(jsonData.length);
-  
-  var correct = [];
-  var wrong = [];
-  
-  for(var i=0; i<jsonData.length; i++){
-    if(jsonData[i].audio_jap.endsWith("_e.mp3")) {
-      wrong.push(jsonData[i]);
-    } else {
-      correct.push(jsonData[i])
-    }
-  }
-  
-  fs.writeFile("correct.json", JSON.stringify(correct, null, 2), function(err) {
-    if(err) {
+fs.readFile('../data/all.json', 'utf8', function (err, data) {
+    if (err) {
         return console.log(err);
     }
+    var jsonData = JSON.parse(data);
+    console.log(jsonData.length);
 
-    console.log("The file was saved!");
-  }); 
-  
-  fs.writeFile("english_audio_sentneces.json", JSON.stringify(wrong, null, 2), function(err) {
-    if(err) {
-        return console.log(err);
+    var correct = [];
+    var wrong = [];
+
+    for (var i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].audio_jap.endsWith("_e.mp3")) {
+            wrong.push(jsonData[i]);
+        } else {
+            correct.push(jsonData[i])
+        }
     }
 
-    console.log("The file was saved!");
-  }); 
-  
-  
-  console.log(wrong.length, correct.length);
+    fs.writeFile("correct.json", JSON.stringify(correct, null, 2), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
+    fs.writeFile("english_audio_sentneces.json", JSON.stringify(wrong, null, 2), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
+
+    console.log(wrong.length, correct.length);
 });
